bb5a1482ab24d638b9492f99d7cfec24a4784f78
Ngwu Maxwell C. <96695018+NgwuMaxwell@users.noreply.github.com>
2026-01-23 00:59:34 +0100

    Fix PUT method not supported for notices update route

--- core/routes/admin.php
+++ core/routes/admin.php
@@ -325,7 +325,7 @@ Route::controller('NoticeController')->name('notices.')->prefix('notices')->grou
     Route::get('create', 'create')->name('create');
     Route::post('store', 'store')->name('store');
     Route::get('edit/{id}', 'edit')->name('edit');
-    Route::post('update/{id}', 'update')->name('update');
+    Route::put('update/{id}', 'update')->name('update');
     Route::get('delete/{id}', 'delete')->name('delete');
     Route::get('status/{id}', 'status')->name('status');
 });
